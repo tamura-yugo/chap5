a61b6c9ad7af570b96889d3dbdf70b305b7d7d73
dodosukoipainapooo@docomo.ne.jp <dodosukoipainapooo@docomo.ne.jp>
2021-09-16 20:13:54 +0900

    tamura

--- upload-input.php
+++ upload-input.php
@@ -1,5 +1,5 @@
 <?php require '../header.php'; ?>
-<p>アップロードするファイルを指定してください。</p>
+<p>アップロード   するファイルを指定してください。</p>
 <form action="upload-output.php" method="post" enctype="multipart/form-data">
 <p><input type="file" name="file"></p>
 <p><input type="submit" value="アップロード"></p>
